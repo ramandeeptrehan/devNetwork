5aba23c9a87874f5de08d5fd4f6756a145388eb7
Raman Trehan <ramandeeptrehan@gmail.com>
2021-01-14 01:42:36 -0800

    log message

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,5 +9,21 @@ const router = express.Router();
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "User works"})); //response being sent is a JSON here
 
+/*
+The reason we need private routes because we want some things for only logged in users to do
+Example: you login and then create a profile
+So, create profile should be part of private route
+
+We don't want any public access to API which can create profile or edit it. They shouldn't be
+able to do things just via POSTMAN. They should get "unauthorized error". That's why we 
+have JSON web tokens. In order to access a private route, you need to send JSON web token along with it,
+The way you get JSON web toke is when you log in and you get it. When trying to access a private route,
+you send this token back to authorize.
+
+Difference between authentication and authorization?
+->We can add
+
+*/
+
 //export router so that server.js can pick it up
 module.exports = router;
