71aac63257a2e4b06ef97d1c4a0f93a5f0159f01
Raman Trehan <ramandeeptrehan@gmail.com>
2021-01-19 00:52:38 -0800

    Add fields to Profile model

--- models/Profile.js
+++ models/Profile.js
@@ -34,5 +34,52 @@ const ProfileSchema = new Schema({
     },
     githubUserName: {
         type: String
-    }
+    },
+    experience: [ //array of objects
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String,
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: { //not making it (required: true) since this could be their current job
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false //default value if false: UI checkbox is unticked, by default
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+           school: {
+               type: String,
+               required: true
+           },
+           degree: {
+               type: String,
+               required: true
+           },
+           fieldOfStudy: {
+               type: String,
+               required: true
+           }
+        }
+    ]
+
+    
 });
